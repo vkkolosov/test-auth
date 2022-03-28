6132b1d107340b33851b1c2ef924d2bccccd2f87
vkolosovvv <madrat444>
2022-03-28 15:34:22 +0300

    init

---
+++ src/test/java/com/hblcak/TestAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hblcak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
